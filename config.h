4447c67620b5e80475c2d596737f2cefc11ca76a
Ng Yik Kai <yikkai95@gmail.com>
2021-08-21 15:33:07 +0800

    [FEAT] hide menubar

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappiv    = 10;       /* vert inner gap between window
 static const unsigned int gappoh    = 100;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 100;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Go Mono Nerd Font:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Go Mono Nerd Font:pixelsize=16:antialias=true:autohint=true";
@@ -78,7 +78,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
